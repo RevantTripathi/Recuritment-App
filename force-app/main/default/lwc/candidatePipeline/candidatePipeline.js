d72b4f6d0a27967e7451c6e35afce236c40effb5
Revant Tripathi <revanttripathi8@gmail.com>
2026-09-16 01:29:50 +0530

    Collection and grouping of candidates in LWC

--- force-app/main/default/lwc/candidatePipeline/candidatePipeline.js
+++ force-app/main/default/lwc/candidatePipeline/candidatePipeline.js
@@ -1,8 +1,25 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getCandidatesForPipeline from '@salesforce/apex/CandidatePipelineController.getCandidatesForPipeline';
 
 
 
 export default class CandidatePipeline extends LightningElement {
-    @wire(getCandidatesForPipeline) candidates;
+    @track groupCandidates = {};
+
+    @wire(getCandidatesForPipeline)
+    wireCandidates({error,data}){
+        if(data){
+            const grouped = [{stageName:'Applied',candidates:[]},{stageName:'Screening', candidates:[]},{stageName:'Interview', candidates:[]},{stageName:'Offer',candidates:[]}];
+            data.forEach((cand) => { 
+                const matchingColumn = grouped.find((col) => col.stageName ===cand.Stage__c);
+                if(matchingColumn){
+                    matchingColumn.candidates.push(cand);
+                }
+            });
+
+            this.groupCandidates = grouped;
+        }else if(error){
+            console.error('Error loading Candidates',error);
+        }
+    };
 }
